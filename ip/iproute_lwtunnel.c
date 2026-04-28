a76c811656f370ac1dfad06d6b869ab2e7784b73
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2026-04-28 21:31:05 +0900

    seg6: add support for the End.M.GTP6.E behavior
    Add support for the End.M.GTP6.E behavior, which translates SRv6
    traffic into IPv6/GTP-U.  The behavior reuses the src and pdu_type
    keywords introduced for End.M.GTP4.E; v4_mask_len is not meaningful
    for an IPv6/GTP-U tunnel and is rejected.
    
    Example:
    ip -6 r a 2001:db8:1::/64 encap seg6local action End.M.GTP6.E \
        src 2001:db8::1 pdu_type ul dev sr0
    
    Link: https://datatracker.ietf.org/doc/html/rfc9433
    
    Signed-off-by: Yuya Kusakabe <yuya.kusakabe@gmail.com>

--- ip/iproute_lwtunnel.c
+++ ip/iproute_lwtunnel.c
@@ -407,6 +407,7 @@ static const char *seg6_action_names[SEG6_LOCAL_ACTION_MAX + 1] = {
 	[SEG6_LOCAL_ACTION_END_DT46]		= "End.DT46",
 	[SEG6_LOCAL_ACTION_END_MAP]		= "End.MAP",
 	[SEG6_LOCAL_ACTION_END_M_GTP4_E]	= "End.M.GTP4.E",
+	[SEG6_LOCAL_ACTION_END_M_GTP6_E]	= "End.M.GTP6.E",
 };
 
 static const char *format_action_type(int action)
