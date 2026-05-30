796a6efe4aa1edcb5d99fd4a0ebc4390bd7897ef
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2026-05-30 13:54:04 +0900

    seg6: add support for the End.M.GTP6.E behavior under seg6mobile encap
    Add parse and print support for the End.M.GTP6.E behavior (RFC 9433)
    in the seg6mobile encap namespace.  End.M.GTP6.E terminates an
    inbound SRv6 packet whose SRH carries the final IPv6 destination as
    the last segment, recovers the GTP-U TEID / QFI / PDU Session ID
    from the SID, and emits an IPv6 / UDP / GTPv1-U packet toward that
    destination, with an optional PDU Session Container extension
    header.
    
    The behavior introduces the src and udp6_zerocsum_tx attributes and
    reuses the pdu_type attribute added by End.M.GTP4.E:
    
      src                  outer IPv6 source address
      pdu_type             4-bit PDU Type ("dl" = 0, "ul" = 1, or a
                           raw 0..15); its presence selects long
                           GTPv1-U + PDU Session Container over short
                           GTPv1-U
      udp6_zerocsum_tx     transmit a zero UDP checksum on the emitted
                           GTP-U packet (RFC 6935); off by default
    
    Configuration:
    
        ip -6 route add 2001:db8:e::/64 \
            encap seg6mobile action End.M.GTP6.E \
                src 2001:db8:2::1 pdu_type dl \
            dev <dev>
    
    Assisted-by: LLM
    Signed-off-by: Yuya Kusakabe <yuya.kusakabe@gmail.com>

--- ip/iproute_lwtunnel.c
+++ ip/iproute_lwtunnel.c
@@ -586,6 +586,7 @@ static void print_encap_seg6local(FILE *fp, struct rtattr *encap)
 static const char *seg6_mobile_action_names[SEG6_MOBILE_ACTION_MAX + 1] = {
 	[SEG6_MOBILE_ACTION_END_MAP]		= "End.MAP",
 	[SEG6_MOBILE_ACTION_END_M_GTP4_E]	= "End.M.GTP4.E",
+	[SEG6_MOBILE_ACTION_END_M_GTP6_E]	= "End.M.GTP6.E",
 };
 
 static const char *seg6_mobile_pdu_type_names[] = {
@@ -694,6 +695,11 @@ static void print_encap_seg6mobile(FILE *fp, struct rtattr *encap)
 		print_string(PRINT_ANY, "mapped_sid", "mapped_sid %s ",
 			     rt_addr_n2a_rta(AF_INET6, tb[SEG6_MOBILE_MAPPED_SID]));
 
+	if (tb[SEG6_MOBILE_SRC_ADDR])
+		print_string(PRINT_ANY, "src", "src %s ",
+			     rt_addr_n2a_rta(AF_INET6,
+					     tb[SEG6_MOBILE_SRC_ADDR]));
+
 	if (tb[SEG6_MOBILE_V6_SRC_PREFIX_LEN])
 		print_uint(PRINT_ANY, "v6_src_prefix_len",
 			   "v6_src_prefix_len %u ",
@@ -709,6 +715,10 @@ static void print_encap_seg6mobile(FILE *fp, struct rtattr *encap)
 			print_uint(PRINT_ANY, "pdu_type",
 				   "pdu_type %u ", pdu_type);
 	}
+
+	if (tb[SEG6_MOBILE_UDP6_ZEROCSUM_TX])
+		print_null(PRINT_ANY, "udp6_zerocsum_tx",
+			   "udp6_zerocsum_tx ", NULL);
 
 	if (tb[SEG6_MOBILE_COUNTERS] && show_stats)
 		print_seg6_mobile_counters(fp, tb[SEG6_MOBILE_COUNTERS]);
@@ -1756,7 +1766,8 @@ static int parse_encap_seg6mobile(struct rtattr *rta, size_t len, int *argcp,
 				  char ***argvp)
 {
 	int action_ok = 0, mapped_sid_ok = 0, counters_ok = 0;
-	int pdu_type_ok = 0, v6_src_prefix_len_ok = 0;
+	int src_ok = 0, pdu_type_ok = 0, v6_src_prefix_len_ok = 0;
+	int udp6_zerocsum_tx_ok = 0;
 	char **argv = *argvp;
 	int argc = *argcp;
 	inet_prefix addr;
@@ -1781,6 +1792,13 @@ static int parse_encap_seg6mobile(struct rtattr *rta, size_t len, int *argcp,
 			get_addr(&addr, *argv, AF_INET6);
 			ret = rta_addattr_l(rta, len, SEG6_MOBILE_MAPPED_SID,
 					    &addr.data, addr.bytelen);
+		} else if (strcmp(*argv, "src") == 0) {
+			NEXT_ARG();
+			if (src_ok++)
+				duparg2("src", *argv);
+			get_addr(&addr, *argv, AF_INET6);
+			ret = rta_addattr_l(rta, len, SEG6_MOBILE_SRC_ADDR,
+					    &addr.data, addr.bytelen);
 		} else if (strcmp(*argv, "pdu_type") == 0) {
 			NEXT_ARG();
 			if (pdu_type_ok++)
@@ -1800,6 +1818,12 @@ static int parse_encap_seg6mobile(struct rtattr *rta, size_t len, int *argcp,
 			ret = rta_addattr8(rta, len,
 					   SEG6_MOBILE_V6_SRC_PREFIX_LEN,
 					   u8val);
+		} else if (strcmp(*argv, "udp6_zerocsum_tx") == 0) {
+			if (udp6_zerocsum_tx_ok++)
+				duparg2("udp6_zerocsum_tx", *argv);
+			ret = rta_addattr_l(rta, len,
+					    SEG6_MOBILE_UDP6_ZEROCSUM_TX,
+					    NULL, 0);
 		} else if (strcmp(*argv, "count") == 0) {
 			if (counters_ok++)
 				duparg2("count", *argv);
